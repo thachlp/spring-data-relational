97ae18718236d4b62949e18558126bcab88eb871
Chirag Tailor <ctailor2@gmail.com>
2022-01-31 14:23:25 -0600

    Add tests to demonstrate the bug with nested mapped collections.

--- spring-data-jdbc/src/test/java/org/springframework/data/jdbc/repository/JdbcRepositoryMappedCollectionIntegrationTests.java
+++ spring-data-jdbc/src/test/java/org/springframework/data/jdbc/repository/JdbcRepositoryMappedCollectionIntegrationTests.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,19 +25,28 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import lombok.Data;
 import lombok.Value;
+import org.springframework.transaction.annotation.Transactional;
 
 // TODO: Should these be moved into  JdbcRepositoryWithCollectionsIntegrationTests?
 @ExtendWith(SpringExtension.class)
+@Transactional
 public class JdbcRepositoryMappedCollectionIntegrationTests {
 	@Autowired EntityWithCollectiblesRepository entityWithCollectiblesRepository;
 
+	@Autowired EntityWithNestedCollectiblesRepository entityWithNestedCollectiblesRepository;
+
 	@Autowired OtherEntityRepository otherEntityRepository;
+
+	private OtherEntity otherEntity = new OtherEntity();
+
+	@BeforeEach
+	void setUp() {
+		otherEntity.name = "someName";
+		otherEntity = otherEntityRepository.save(otherEntity);
+	}
 
 	@Test
 	void mapsCollectionsAsSet() {
-		OtherEntity otherEntity = new OtherEntity();
-		otherEntity.name = "someName";
-		otherEntity = otherEntityRepository.save(otherEntity);
 
 		EntityWithCollectibles entity = new EntityWithCollectibles();
 		SetCollectible setCollectible = new SetCollectible();
@@ -71,6 +81,98 @@ public class JdbcRepositoryMappedCollectionIntegrationTests {
 
 		EntityWithCollectibles reloaded = entityWithCollectiblesRepository.findById(entity.id).get();
 		assertThat(reloaded.mapCollectibles.get("someName")).isEqualTo(mapCollectible);
+	}
+
+	@Test
+	void mapsCollectionsNestedUnderListElement() {
+		EntityWithNestedCollectibles entity = new EntityWithNestedCollectibles();
+		RootListCollectible rootListCollectible = new RootListCollectible();
+		NestedCollectible nestedCollectible = new NestedCollectible("someName");
+		rootListCollectible.nestedCollectibles = singletonList(nestedCollectible);
+		entity.rootListCollectibles = singletonList(rootListCollectible);
+
+		entity = entityWithNestedCollectiblesRepository.save(entity);
+
+		EntityWithNestedCollectibles reloaded = entityWithNestedCollectiblesRepository.findById(entity.id).get();
+		assertThat(reloaded.rootListCollectibles).hasSize(1);
+		assertThat(reloaded.rootListCollectibles.get(0).nestedCollectibles).containsExactly(nestedCollectible);
+	}
+
+	@Test
+	void mapsCollectionsNestedUnderSetElement() {
+		EntityWithNestedCollectibles entity = new EntityWithNestedCollectibles();
+		RootSetCollectible rootSetCollectible = new RootSetCollectible();
+		NestedCollectible nestedCollectible = new NestedCollectible("someName");
+		rootSetCollectible.otherEntityId = AggregateReference.to(otherEntity.id);
+		rootSetCollectible.nestedCollectibles = singletonList(nestedCollectible);
+		entity.rootSetCollectibles = singleton(rootSetCollectible);
+
+		entity = entityWithNestedCollectiblesRepository.save(entity);
+
+		EntityWithNestedCollectibles reloaded = entityWithNestedCollectiblesRepository.findById(entity.id).get();
+		assertThat(reloaded.rootSetCollectibles).hasSize(1);
+		assertThat(reloaded.rootSetCollectibles.stream().findFirst().get().nestedCollectibles).containsExactly(nestedCollectible);
+	}
+
+	@Test
+	void mapsCollectionsNestedUnderMapElement() {
+		EntityWithNestedCollectibles entity = new EntityWithNestedCollectibles();
+		RootMapCollectible rootMapCollectible = new RootMapCollectible();
+		NestedCollectible nestedCollectible = new NestedCollectible("someName");
+		rootMapCollectible.value = 123;
+		rootMapCollectible.nestedCollectibles = singletonList(nestedCollectible);
+		entity.rootMapCollectibles = singletonMap("someKeyName", rootMapCollectible);
+
+		entity = entityWithNestedCollectiblesRepository.save(entity);
+
+		EntityWithNestedCollectibles reloaded = entityWithNestedCollectiblesRepository.findById(entity.id).get();
+		assertThat(reloaded.rootMapCollectibles.get("someKeyName").nestedCollectibles).hasSize(1);
+		assertThat(reloaded.rootMapCollectibles.get("someKeyName").nestedCollectibles).containsExactly(nestedCollectible);
+	}
+
+	interface EntityWithNestedCollectiblesRepository extends CrudRepository<EntityWithNestedCollectibles, Long> {}
+
+	static class EntityWithNestedCollectibles {
+		@Id Long id;
+
+		@MappedCollection(idColumn = "ENTITY_WITH_NESTED_COLLECTIBLES_ID", keyColumn = "INDEX")
+		List<RootListCollectible> rootListCollectibles;
+
+		@MappedCollection(idColumn = "ENTITY_WITH_NESTED_COLLECTIBLES_ID", keyColumn = "OTHER_ENTITY_ID")
+		Set<RootSetCollectible> rootSetCollectibles;
+
+		@MappedCollection(idColumn = "ENTITY_WITH_NESTED_COLLECTIBLES_ID", keyColumn = "NAME")
+		Map<String, RootMapCollectible> rootMapCollectibles;
+	}
+
+	static class RootListCollectible {
+//		@Id Long id; // Adding this causes test to pass, but should not be necessary
+
+		@MappedCollection(idColumn = "ROOT_COLLECTIBLE_ID", keyColumn = "INDEX")
+		List<NestedCollectible> nestedCollectibles;
+	}
+
+	static class RootSetCollectible {
+//		@Id Long id; // Adding this causes test to pass, but should not be necessary
+
+		AggregateReference<OtherEntity, Long> otherEntityId;
+
+		@MappedCollection(idColumn = "ROOT_COLLECTIBLE_ID", keyColumn = "INDEX")
+		List<NestedCollectible> nestedCollectibles;
+	}
+
+	static class RootMapCollectible {
+//		@Id Long id; // Adding this causes test to pass, but should not be necessary
+
+		Integer value;
+
+		@MappedCollection(idColumn = "ROOT_COLLECTIBLE_ID", keyColumn = "INDEX")
+		List<NestedCollectible> nestedCollectibles;
+	}
+
+	@Value
+	static class NestedCollectible {
+		String name;
 	}
 
 	interface EntityWithCollectiblesRepository extends CrudRepository<EntityWithCollectibles, Long> {}
@@ -129,6 +231,11 @@ public class JdbcRepositoryMappedCollectionIntegrationTests {
 		@Bean
 		OtherEntityRepository otherEntityRepository() {
 			return factory.getRepository(OtherEntityRepository.class);
+		}
+
+		@Bean
+		EntityWithNestedCollectiblesRepository entityWithNestedCollectiblesRepository() {
+			return factory.getRepository(EntityWithNestedCollectiblesRepository.class);
 		}
 	}
 }
